a1ef47f7b18f99d5a50342914bce76a62442c935
Steve <steven.starwalt@yahoo.com>
2018-01-11 14:59:09 -0800

    added more comments in the code

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
-    
+//get the userNameInput    
 alert('Hi! You don\'t know me, but after this we will know a little about each other.  Now let the fun begin!');
 var userNameInput = prompt('My name is Steve, what is your name?');
 console.log('The users name is:' + userNameInput);
 
 alert('Nice to meet you ' + userNameInput + '. Now we\'re going to play a simple yes-no question game so you can learn a fun fact about me. Please answer the following questions with yes or no only');
 
+//question 1 northPole
 var poleCounter = 0;
 
 var northPole = prompt(userNameInput + ' if I told you I have been to the top of the world, would you believe me?').toLowerCase();
@@ -20,6 +21,7 @@ if(northPole === 'yes') {
 }
 console.log('The user knows I\'ve been to the North Pole in 1996');
 
+//question 2 notCold
 var notCold = prompt('Would you believe I got all the way there without ever getting cold?').toLowerCase();
 if (notCold === 'yes') {
   alert('Right, and I wasn\'t even wearing a coat!');
@@ -29,6 +31,7 @@ if (notCold === 'yes') {
 }
 console.log('The user has knows I stayed warm');
 
+//question 3 fly
 var fly = prompt('If I said I flew over the Polar Ice cap to get there, would you believe that?').toLowerCase();
 if (fly === 'yes') {
   alert('No I didn\'t fly! I don\'t like planes.');      
@@ -38,6 +41,7 @@ if (fly === 'yes') {
 }
 console.log('The user has knows I did\'t fly');
 
+//question 4 transporter
 var transporter = prompt('Could I have been transported there like Spock or Captian Kirk would have?').toLowerCase();
 if (transporter === 'no') {
   alert('Yes, transpoters don\'t exist. Star Trek was just a TV show.');
@@ -47,6 +51,7 @@ if (transporter === 'no') {
 }
 console.log('The user understands Star Trek was just a TV show');
 
+//question 5 submarine
 var submarine = prompt('Well, I didn\'t fly and I wasn\'t magically transported. If I told you I got there by going under the ice in a submarine, would you believe that?').toLowerCase();
 if (submarine === 'yes') {
   alert('Fantastic! In 1996 I traveled under the polar ice cap in the relative warmth of a nuclear submarine and surfaced at the North Pole.');
@@ -55,6 +60,7 @@ if (submarine === 'yes') {
   alert('Sorry wrong answer. In 1996 I traveled under the polar ice cap in the relative warmth of a nuclear submarine and surfaced at the North Pole.');
 }
 
+//question 6 setup
 alert('Well, ' + userNameInput + ' thanks for playing that little game.  As I said, we now know a little about each other.  I know your name, and you know I spent some time on a submarine and traveled under the ice to the North Pole in 1996.  This voyage was done as part 2 of a 6 part unclassified joint military and civilian mission known as Scicex (Science Exercise).  The mission of Scicex was to study the efects of climate change on the polar ice cap and the Arctic ocean. Later I\'ll share some pictures of that trip.');
 
 var statesLivedIn = 11;
@@ -65,7 +71,7 @@ var flag = false;
 
 alert('As you may have guessed I was in the Navy, so have lived in many places. I also moved many times as a child and after I retired from the Navy. I\'m going to give you 4 tries to guess how amny states I lived in.');
 
-
+//Question 6 statesLivedIn
 while (flag === false && counter < 5) {
   var statesGuess = prompt('What is your guess?');
   statesGuess = parseInt(statesGuess);
@@ -88,10 +94,12 @@ if (counter === 5) {
 }
 console.Log('The user knows how many states I lived in.');
 
+//question 7 setup
 alert('Let\'s keep with the places I\'ve lived theme. I\'ve lived in 3 countries besides the United States. I can tell one of these countries was in the Eastern Hemisphere, two were in the Southern Hemisphere.  In two of these countries English was not the primary language, and Portugese was the primary language in one of these countries.')
 
 var countries = ['mexico', 'brazil', 'australia'];
 
+//question 7 countries
 var countriesGuess = prompt('Can you guess one of the countries besides the United States I\'ve lived in?').toLowerCase();
 
 var countriesCounter = 0;
@@ -108,8 +116,9 @@ for(var i = 0; i < countries.length; i++) {
 }
 console.Log('The user knows which countries.');
 
+//grade the quizes
 if(counter < 5 && countriesCounter === 0) {
-  alert(userNameInput + ' now we\'re almost good friends.  Let\'s review how you did on my little quizes.  You got ' + poleCounter + ' of the 5 yes or no answers correct. It took you ' + counter + ' of the possible 4 guesses to guess how many ststes I lived in. Finally, you did guess one of the countries besides the United States I lived in.  Overall great job!');
+  alert(userNameInput + ' now we\'re almost best friends now!  Let\'s review how you did on my little quizes.  You got ' + poleCounter + ' of the 5 yes or no answers correct. It took you ' + counter + ' of the possible 4 guesses to guess how many ststes I lived in. Finally, you did guess one of the countries besides the United States I lived in.  Overall great job!');
 }
 else if(counter < 5 && countriesCounter === 1) {
   alert(userNameInput + ' now we\'re almost good friends.  Let\'s review how you did on my little quizes.  You got ' + poleCounter + ' of the 5 yes or no answers correct. It took you ' + counter + ' of the possible 4 guesses to guess how many states I lived in. Finally, you did not guess one of the countries besides the United States I lived in.  Overall good job!');
